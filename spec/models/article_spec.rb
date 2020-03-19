7a4337761cbdc82db0e1bc9a7552324426cd30b0
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-19 20:13:59 +0100

    added tests for articles model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'should have valid Factory' do
+    expect(create(:article)).to be_valid
+  end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :teaser }
+    it { is_expected.to have_db_column :content }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :title}
+    it { is_expected.to validate_presence_of :teaser }
+    it { is_expected.to validate_presence_of :content } 
+  end
+
+  describe "Check if new article is created" do
+    before do
+      @article = Article.new(title: "Corona is viral", teaser: "Is there something you can do?", content: "NO. sorry there isn't.")
+    end  
+
+    it "expects to contain title" do
+    expect(@article.title).to eq("Corona is viral")
+    end
+    it "expects to contain the description" do      
+      expect(@article.teaser).to eq("Is there something you can do?")
+    end
+    it "expects to contain the description" do      
+      expect(@article.content).to eq("NO. sorry there isn't.")
+    end
+  end
 end
