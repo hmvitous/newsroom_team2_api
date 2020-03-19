fb91aa6a3cc7b6676ec0198d0c048ccba0ad229b
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-19 14:02:17 +0100

    added rake-cors and started to build the index function

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,16 @@ module NewsroomTeam2Api
       generate.routing_specs false
       generate.controller_specs false
     end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+          headers: :any, 
+          methods: %i[get post put delete],
+          expose: %w(access-token expiry token-type uid client),
+          max_age: 0
+      end
+    end
   end
 end
+
