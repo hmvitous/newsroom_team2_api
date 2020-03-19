95f052646419d48d773400ad42e9f9d9e1437ba9
Anish2504 <anish.kanswal@gmail.com>
2020-03-19 14:55:11 +0100

    created articles model

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,4 +2,9 @@ class Api::ArticlesController < ApplicationController
   def index
     render json: {header: 'Pong', content:'blablabla.', teaser: "I'm your father."}, status: 200
   end
+
+  def create
+    render plain: params [:articles].inspect
+  end
+
 end
