0626aa78c4c2263ec22e875a9542c2f5363efc71
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-20 16:03:17 +0100

    trying to fix specific article tests

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,19 +1,19 @@
 class Api::ArticlesController < ApplicationController
   def index
     collection_articles = Article.all
-     if collection_articles.empty?
-      render json: { error_message: "No articles has been found" }, status: 404
-     else
+    if collection_articles.empty?
+      render json: { error_message: 'No articles has been found' }, status: 404
+    else
       render json: { articles: collection_articles }, status: 200
-    end
+   end
   end
-  
+
   def show
     article = Article.find(params[:id])
     if article.nil?
-      render json: {error_message: "Article doesn't exist"}, status: 404
+      render json: { error_message: "Article doesn't exist" }, status: 404
     else
-      render json: {id: article.id, title: article.title, teaser: article.teaser, content: article.content}, status: 200
+      render json: article, status: 200
     end
   end
 end
--- spec/controllers/user_can_see_a_specific_article_spec.rb
+++ spec/controllers/user_can_see_a_specific_article_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe 'Get/api/articles/', type: :request do
   describe 'GET/articles' do
     @article = Article.create(title: "Corona is viral", 
@@ -8,25 +6,39 @@ RSpec.describe 'Get/api/articles/', type: :request do
         teaser: "We are saved?", content: "NO! Recession is coming next!")
         
     before do
+      # create(:article, title: "coronavirus", teaser: "The World goverments cannot manage it", content: "it's spreading all around the world")
       get '/api/articles/2'
     end
 
     it 'should return a 200 response' do
       expect(response.status).to eq 200
+    end
+
+    it 'should return id 2' do
+      expect((response_json)["id"]).to eq 2
+    end
+
+    it 'should return valid title' do
+      expect((response_json)["title"]).to eq "Corona is defeated!!"
+    end
+
+    it 'should return valid teaser' do
+      expect((response_json)["teaser"]).to eq "We are saved?"
+    end
+
+    it 'should return valid teaser' do
+      expect((response_json)["content"]).to eq "NO! Recession is coming next!"
+    end
+  end
+
+  describe 'GET/articles [sad path]' do
+    before do
+      get '/api/articles/10000000'
+    end
+
+    it 'should return a 404 response' do
       binding.pry
+      expect(response.status).to eq 404
     end
-
-    it 'should return a 200 response' do
-      expect(response.id).to eq 200
-    end
-
-    it 'should return a 200 response' do
-      expect(response.title).to eq 200
-    end
-
-    it 'should return a 200 response' do
-      expect(response.title).to eq 200
-    end
-
   end
 end
