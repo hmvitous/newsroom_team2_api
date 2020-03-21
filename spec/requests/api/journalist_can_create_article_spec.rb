a179ebe42da8c7edce0ff1183be7ccd0e54789d3
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-21 22:09:22 +0100

    Removed binding.pry, moved user_specific_spec where it belongs

--- spec/requests/api/journalist_can_create_article_spec.rb
+++ spec/requests/api/journalist_can_create_article_spec.rb
@@ -7,22 +7,18 @@ RSpec.describe Api::ArticlesController, type: :request do
     before do
       post '/api/articles/create',
            params: {
-             article: {
-               title: 'Coronavirus',
-               teaser: 'Things are bad',
-               content: 'It will get worse.'
-             }
+             title: 'Coronavirus',
+             teaser: 'Things are bad',
+             content: 'It will get worse.'
            },
            headers: headers
     end
 
     it 'returns a 200 response status' do
-      binding.pry
       expect(response.status).to eq 200
     end
 
     it 'succesfully creates Article entry' do
-      binding.pry
       entry = Article.last
       expect(entry.title).to eq 'Coronavirus'
     end
