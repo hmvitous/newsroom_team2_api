e89541dccad681760ada4ea606ba154664e7254c
Hunter <hmvitous@yahoo.com>
2020-03-27 10:00:20 +0100

    Creates a subscriber role in the user model and creates tests for happy and sad path

--- spec/requests/api/users_can_login_spec.rb
+++ spec/requests/api/users_can_login_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe 'POST /auth/sign_in', type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
   let(:user) { create(:user) }
+  let(:subscriber) { create(:user, role: "subscriber") }
   let(:expected_response) do
     {
       'data' => {
@@ -27,7 +28,7 @@ RSpec.describe 'POST /auth/sign_in', type: :request do
     end
 
     it 'returns 200 response status' do
-      expect(response). to have_http_status 200
+      expect(response).to have_http_status 200
     end
 
     it 'returns the expected response' do
@@ -54,12 +55,46 @@ RSpec.describe 'POST /auth/sign_in', type: :request do
     end
   end
 
-  describe 'with invaled email' do
+  describe 'with invalid email' do
     before do
       post '/api/auth/sign_in',
            params: {
              email: 'wrong@email.com',
              password: user.password
+           },
+           headers: headers
+    end
+
+    it 'returns 401 reponse status' do
+      expect(response).to have_http_status 401
+    end
+
+    it 'returns error message' do
+      expect(response_json['errors']).to eq ['Invalid login credentials. Please try again.']
+    end
+  end
+
+  describe 'subscriber can login' do
+    before do
+      post '/api/auth/sign_in',
+      params: {
+        email: subscriber.email,
+        password: subscriber.password
+      },
+      headers: headers
+    end
+
+    it 'returns 200 response status' do
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'with invalid email' do
+    before do
+      post '/api/auth/sign_in',
+           params: {
+             email: 'wrong@email.com',
+             password: subscriber.password
            },
            headers: headers
     end
