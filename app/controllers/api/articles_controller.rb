0c85c4b2d3fa9e85fca30e77446dc97b6ecedaa1
bounce <johan@waytogo.se>
2020-04-03 16:12:10 +0200

    added :image as permitted params in articles controller

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -50,6 +50,6 @@ class Api::ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :teaser, :content, :article_class)
+    params.require(:article).permit(:title, :teaser, :content, :article_class, :image)
   end
 end
