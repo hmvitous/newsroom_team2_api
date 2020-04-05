a89a8d19f50cda10ce37b67e31a4b9c76ca50d5b
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-05 12:58:21 +0200

    [FIXES] Issue with message on internal page not showing the correct message when an article is created fixed.

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -22,10 +22,12 @@ class Api::ArticlesController < ApplicationController
     article = Article.create(article_params)
     last_article = Article.last
     new_time = Time.now.strftime('%d-%m-%Y')
-    if article.persisted? && attach_image(article)
+    if article.persisted? && !attach_image(article)
       last_article[:new_created_at] = new_time
       last_article.save
       render json: { message: 'Your article is ready for review.' }, status: 200
+    elsif article.persisted? && attach_image(article)
+    render json: { message: 'Your article is ready for review.' }, status: 200
     else
       render json: { message: 'Your article was not saved.' }, status: 406
     end
